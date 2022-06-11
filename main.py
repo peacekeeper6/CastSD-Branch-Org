56ae94bcb4a591c4c83aeba23c519d449ad1d66a
Jun <87632049+peacekeeper6@users.noreply.github.com>
2022-06-11 15:41:41 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# import "packages" from flask
+from flask import Flask, render_template
+
+# create a Flask instance
+app = Flask(__name__)
+
+
+# connects default URL to render index.html
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/placeholder/')
+def placeholder():
+    return render_template("placeholder.html")
+
+# runs the application on the development server
+if __name__ == "__main__":
+    app.run(debug=True)
