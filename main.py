e9aa422600737813fe89de81a9f5d71a8c7a46fe
peacekeeper6 <junlim954@gmail.com>
2022-06-12 23:08:02 -0700

    crud

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# import "packages" from flask
-from flask import Flask, render_template
+import markdown
+from flask import render_template
+from __init__ import app
+from flask_login import current_user
+from usercrud.query import user_by_id
+
+from usercrud.app_crud import app_crud
+
+app.register_blueprint(app_crud)
 
 # create a Flask instance
 app = Flask(__name__)
@@ -10,13 +17,17 @@ app = Flask(__name__)
 def index():
     return render_template("layouts/index.html")
 
-@app.route('/aboutkoinonia/')
+@app.route('/aboutkoinonia')
 def aboutkoinonia():
     return render_template("aboutus/aboutkoinonia.html")
 
-@app.route('/getinvolved/')
+@app.route('/getinvolved')
 def getinvolved():
     return render_template("getinvolved.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
 @app.errorhandler(404)
 def error(e):
